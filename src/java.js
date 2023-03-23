56d02c61e2d1d1357f6a0c866a8f3eb980539983
negartourany <negartoorany283@gmail.com>
2023-03-23 09:05:30 +0100

    finished setting date and time

--- src/java.js
+++ src/java.js
@@ -1,3 +1,25 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemp(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -10,6 +32,8 @@ function displayTemp(response) {
   humid.innerHTML = response.data.main.humidity;
   let windStat = document.querySelector("#wind-stat");
   windStat.innerHTML = Math.round(response.data.wind.speed);
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "654b4feb287b99dec3603cc2d01ca282";
